78accbaf6b7bc2bd3e6695cbd0f060de2b6a682a
5an528 <sp.sr@icloud.com>
2022-02-08 16:02:25 +0600

    ALL TC DONE

---
+++ Pages/HomePage.cs
@@ -0,0 +1,167 @@
+﻿using EbayDemo.Pages.HomePageElements;
+using EbayDemo.Pages.HomePageElements.PageObjects;
+using EbayDemo.Pages.PopularCategories.PageObjects;
+using EbayDemo.Tests;
+using EbayDemo.Utils;
+using EbayDemo.Utils.PageObjects;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace EbayDemo.Pages.PageObjects
+{
+    public class HomePage
+    {
+        String homePageUrl = Locators.homePageUrl;
+
+        private IWebDriver __driver;
+        private WebDriverWait wait;
+
+        public HomePage(IWebDriver __driver)
+        {
+            this.__driver = __driver;
+            wait = new WebDriverWait(__driver, TimeSpan.FromSeconds(10));
+            PageFactory.InitElements(__driver, this);
+        }
+
+        [FindsBy(How = How.LinkText, Using = "Watchlist")]
+        [CacheLookup]
+        private IWebElement watchListBtn;
+
+        [FindsBy(How = How.XPath, Using = "//div[@id='gh-wl-click-flyout']")]
+        [CacheLookup]
+        private IWebElement watchListVerifyText;
+
+        [FindsBy(How = How.LinkText, Using = "Sign in")]
+        [CacheLookup]
+        private IWebElement signInBtn;
+
+
+        public void GoToPage() => __driver.Navigate().GoToUrl(homePageUrl);
+
+        public LoginPage ClickOnSignIn() { 
+            signInBtn.Click();
+
+            return new LoginPage(__driver);
+        }
+
+        public void ClickOnWatchListBtn() => watchListBtn.Click();
+
+        public void VerifyNoWatchList()
+        {
+            String pageText = watchListVerifyText.Text;
+
+            Assert.AreEqual(pageText, "Looks like you are not watching any items yet.");
+
+            Console.WriteLine("Successfully visit " + pageText);
+        }
+
+        [FindsBy(How = How.XPath, Using = Locators.sneakers)]
+        [CacheLookup]
+        private IWebElement sneakersBtn;
+
+        public Sneakers CliickOnSneakersBtn()
+        {
+            CommonMethods commonMethods = new CommonMethods(__driver);
+
+            commonMethods.ScrollToElement(sneakersBtn);
+            Thread.Sleep(Locators.timeCount);
+            sneakersBtn.Click();
+
+            return new Sneakers(__driver);
+        }
+
+        [FindsBy(How = How.XPath, Using = Locators.koreanBeauty)]
+        [CacheLookup]
+        private IWebElement koreanBeautyBtn;
+
+        public KoreanBeauty ClickOnKoreanBeauty()
+        {
+            CommonMethods commonMethods = new CommonMethods(__driver);
+
+            commonMethods.ScrollToElement(koreanBeautyBtn);
+            Thread.Sleep(Locators.timeCount);
+            koreanBeautyBtn.Click();
+
+            return new KoreanBeauty(__driver);
+        }
+
+        [FindsBy(How = How.XPath, Using = Locators.wristWatches)]
+        [CacheLookup]
+        private IWebElement wristWatchesBtn;
+
+        public WristWatches ClickOnWristWatches()
+        {
+            CommonMethods commonMethods = new CommonMethods(__driver);
+
+            commonMethods.ScrollToElement(wristWatchesBtn);
+            Thread.Sleep(Locators.timeCount);
+            wristWatchesBtn.Click();
+
+            return new WristWatches(__driver);
+        }
+
+        [FindsBy(How = How.XPath, Using = Locators.fishing)]
+        [CacheLookup]
+        private IWebElement fishingBtn;
+        public Fishing ClickOnFishing()
+        {
+            CommonMethods commonMethods = new CommonMethods(__driver);
+
+            commonMethods.ScrollToElement(fishingBtn);
+            Thread.Sleep(Locators.timeCount);
+            fishingBtn.Click();
+
+            return new Fishing(__driver);
+        }
+
+        [FindsBy(How = How.XPath, Using = Locators.collectibles)]
+        [CacheLookup]
+        private IWebElement collectiblesBtn;
+        public Collectibles ClickOnCollectibles()
+        {
+            CommonMethods commonMethods = new CommonMethods(__driver);
+
+            commonMethods.ScrollToElement(collectiblesBtn);
+            Thread.Sleep(Locators.timeCount);
+            collectiblesBtn.Click();
+
+            return new Collectibles(__driver);
+        }
+
+        [FindsBy(How = How.XPath, Using = Locators.smartPhones)]
+        [CacheLookup]
+
+        private IWebElement smartPhonesBtn;
+        internal SmartPhones ClickOnSmartPhones()
+        {
+            CommonMethods commonMethods = new CommonMethods(__driver);
+
+            commonMethods.ScrollToElement(smartPhonesBtn);
+            Thread.Sleep(Locators.timeCount);
+            smartPhonesBtn.Click();
+
+            return new SmartPhones(__driver);
+        }
+
+        [FindsBy(How = How.XPath, Using = Locators.popularSell)]
+        [CacheLookup]
+        private IWebElement popularSellBtn;
+
+        public PopularSell ClickOnPopularSell()
+        {
+            CommonMethods commonMethods = new CommonMethods(__driver);
+
+            commonMethods.ScrollToElement(popularSellBtn);
+            Thread.Sleep(Locators.timeCount);
+            popularSellBtn.Click();
+
+            return new PopularSell(__driver);
+        }
+    }
+}
---
+++ Utils/Locators.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EbayDemo.Utils
+{
+    class Locators
+    {
+        public static int timeCount = 2000;
+        public static String homePageUrl = "https://www.rokomari.com";
+        public static String fbLoginPageTitle = "Log in to Facebook | Facebook";
+
+        //homePageTests
+        public const String deals = "/html/body/header/div/nav/div/div[1]/ul/li[1]/a";
+        public const String customerServiceId = "/html/body/header/div/nav/div/div[1]/ul/li[2]/a";
+        public const String dealsText = "/html/body/header/div/nav/div/div[1]/ul/li[3]/a";
+        public const String customerServicesText = "/html/body/header/div/nav/div/div[1]/ul/li[4]/a";
+        public const String sellingPath = "/html/body/header/div/nav/div/div[1]/ul/li[5]/a";
+        public const String sellText = "/html/body/header/div/nav/div/div[1]/ul/li[6]/a";
+        public const String electronicsText = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[1]/a";
+        public const String fashionPath = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[2]/a";
+        public const String fashionText = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[3]/a";
+        public const String healthAndBeautyPath = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[4]/a";
+        public const String healthAndBeautyText = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[5]/a";
+        public const String homeAndGardenPath = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[6]/a";
+        public const String homeAndGardenText = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[7]/a";
+        public const String sportsPath = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[8]/a";
+        public const String sportsText = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[9]/a";
+        public const String collectiblesAndArtPagePath = "/html/body/header/div/nav/div/div[2]/div/div/div/ul[1]/li[10]/a";
+
+        //popular categories
+        public const String sneakers = "//*[@id='destinations_list1']/ul/li[1]";
+        public const String koreanBeauty = "//*[@id='destinations_list1']/ul/li[2]";
+        public const String wristWatches = "//*[@id='destinations_list1']/ul/li[3]";
+        public const String fishing = "//*[@id='destinations_list1']/ul/li[4]";
+        public const String collectibles = "//*[@id='destinations_list1']/ul/li[5]";
+        public const String smartPhones = "//*[@id='destinations_list1']/ul/li[6]";
+        public const String popularSell = "//*[@id='destinations_list1']/ul/li[7]";
+
+        public const String sneakersUrl = "https://www.ebay.com/b/Retail-Campaign/bn_7116433159";
+        public const String koreanBeautyUrl = "https://www.ebay.com/b/Korean-Health-and-Beauty/bn_7116437158";
+        public const String wristWatchesUrl = "https://www.ebay.com/b/Wristwatches/31387/bn_2408451";
+        public const String fishingUrl = "https://www.ebay.com/b/Fishing-Equipment-Supplies/1492/bn_1851047";
+        public const String collectiblesUrl = "https://www.ebay.com/b/Collectibles-Art/bn_7000259855";
+        public const String smartPhonesUrl = "https://www.ebay.com/b/Cell-Phones-Smart-Watches-Accessories/15032/bn_1865441";
+        public const String popularSellUrl = "https://export.ebay.com/ru/";
+
+        //Sell
+        public const String startSellingPageUrl = "https://www.ebay.com/sl/sell";
+        public const String learnToSellPageUrl = "https://pages.ebay.com/seller-center/get-started/selling-basics.html";
+        public const String affiliatesPageUrl = "https://partnernetwork.ebay.com/";
+
+        //Search Product
+
+        public const String searchBox = "//*[@id='gh-ac']";
+        public const String searchBtn = "//*[@id='gh-btn']";
+        public const String searchPageTextPath = "//*[@id='mainContent']/div[1]/div/div[2]/div[1]/div[1]/h1/span[2]";
+        public const String searchPageText = "mobile";
+        public const String categories = "gh-cat";
+        public const String categoryNameHealthAndBeauty = "//*[@id='gh-cat']/option[20]";
+
+        //Sign In
+        public const String signIn = "Sign in";
+    }
+}
